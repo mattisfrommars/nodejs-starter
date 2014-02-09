7fac54c3300b4a8246b351167c49f9519e1a0876
Matt Jewell <m@mattjewell.co.uk>
2014-02-09 11:47:28 +0000

    absolute paths for configuration files

--- configuration/index.js
+++ configuration/index.js
@@ -3,9 +3,9 @@ var nconf = require( 'nconf' );
 var Config = function ConfigConstructor() {
   nconf.argv().env( '_' );
   var environment = nconf.get( 'NODE:ENV' ) || 'development';
-  nconf.file( environment, 'config/' + environment + '.json' );
-  nconf.file( 'routes', 'config/routes.json' );
-  nconf.file( 'default', 'config/default.json' );
+  nconf.file( environment, absPath('/config/' + environment + '.json') );
+  nconf.file( 'routes', absPath('/config/routes.json') );
+  nconf.file( 'default', absPath('/config/default.json') );
 };
 
 Config.prototype.get = function ( key ) {
